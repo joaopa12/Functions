6e570248e9c832c0204fef4def5943a0cb63de76
Joao <joao10vr@gmail.com>
2021-03-08 16:58:14 -0300

    refactor await

--- public/await.js
+++ public/await.js
@@ -14,15 +14,15 @@ async function printAll1() {
        await printDouble(20);
 }
 
-/*printAll1()*/
+printAll1()
 
 function sumNumbers(number,number2){
     return new Promise(function(resolver){
         setTimeout(function(){
             const sum = (number * 2) + number2
             console.log(sum)
-            resolver(sum)
-        },500)
+            resolver(sum) 
+        }, Math.floor(Math.random() * 100) + 1)
     })
 }
 
@@ -32,4 +32,4 @@ async function printAll2(){
     await sumNumbers(20,25)
 }
 
-printAll2()
+/*printAll2()*/
