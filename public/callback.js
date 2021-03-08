7ffab345b6afbf02b15cc9aabbc081656c69dcb6
Joao <joao10vr@gmail.com>
2021-03-08 20:32:14 -0300

    callback fix async

--- public/callback.js
+++ public/callback.js
@@ -1,65 +1,77 @@
 function printDouble(number, callback){ 
     setTimeout(
        function(){
-            callback( null, number * 2 )
+            let double = number * 2
+            callback( null, double )
        },
-       Math.floor(Math.random() * 100) + 1
-    )
-    console.log(
-        `O dobro do numero ${number}`
+       Math.floor(Math.random() * 1000) + 1
     )
 }
 
 function printAll1(){
-    printDouble(5 , function (error, number) {
+    printDouble(5 , function (error, double) {
         if (error) {
             console.log(error)
         }
-        console.log(number)
+        console.log(double)
+        printDouble(10 , function (error, double) {
+            if (error) {
+                console.log(error)
+            }
+            console.log(double)
+            printDouble(15 , function (error, double) {
+                if (error) {
+                    console.log(error)
+                }
+                console.log(double)
+                printDouble(20 , function (error, double) {
+                    if (error) {
+                        console.log(error)
+                    }
+                    console.log(double)
+                    printDouble(25 , function (error, double) {
+                        if (error) {
+                            console.log(error)
+                        }
+                        console.log(double)
+                    }) 
+                }) 
+            }) 
+        }) 
     }) 
-    printDouble(10 , function (error, number) {
-        if (error) {
-            console.log(error)
-        }
-        console.log(number)
-    }) 
-    printDouble(15 , function (error, number) {
-        if (error) {
-            console.log(error)
-        }
-        console.log(number)
-    })
-    printDouble(20 , function (error, number) {
-        if (error) {
-            console.log(error)
-        }
-        console.log(number)
-    })  
 }
 
 /*printAll1()*/
 
 function sumNumber(number,number2,callback){ 
-    const sum = (number * 2) + number2
+    const sumNumber = (number * 2) + number2
     setTimeout(
-       function(){ 
-            callback( null, sum )
+       function(){
+            console.log(sumNumber)
+            callback( null, sumNumber )
        },
-       0
-    )
-    console.log(
-        `O dobro do numero ${number} mais ${number2} é igual a ${sum}`
+       Math.floor(Math.random() * 100) + 1
     )
 }
 
 function printAll2(){
-    sumNumber(2,4,function(error, sum){
+    let result;
+
+    result = sumNumber(5,0,function(error,result){
         if (error) {
             console.log(error)
         }
-        console.log(
-            `Resultado é ${sum}`
-        )
+        result = sumNumber(12,result,function(error,result){
+            if (error) {
+                console.log(error)
+            }
+
+            result = sumNumber(2,result,function(error,result){
+                if (error) {
+                    console.log(error)
+                }
+            })
+        })
     })
 }
 
